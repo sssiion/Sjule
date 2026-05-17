82aacae3156d51fbd6af7b83715f2deb6a6241f3
sssiion <rta2752775@gmail.com>
2026-05-17 22:34:09 +0900

    chore: 깃허브 404 오류 수정

--- my-payroll/vite.config.js
+++ my-payroll/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/Sjule/',
+  base: process.env.NODE_ENV === 'production' ? '/Sjule/' : '/',
   plugins: [
     tailwindcss(),
     react(),
